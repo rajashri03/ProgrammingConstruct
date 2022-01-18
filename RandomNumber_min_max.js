ec0a26b0e5ee5920123d8d1edd2c696f7c332ace
rajashri03 <telvekar.rajashri@gmail.com>
2022-01-18 15:29:34 +0530

    [Rajashri]Add:Read 5 random number and find Minimum and Maximum value

---
+++ RandomNumber_min_max.js
@@ -0,0 +1,7 @@
+//*Read 5 random number and find mininum and maximum value
+var randoms = [...Array(5)].map(() => Math.floor(Math.random() * 1000));
+console.log(randoms);
+var min = Math.min( ...randoms ),
+   max = Math.max( ...randoms );
+console.log("Minimum Value:"+min);
+console.log("Maximum Value:"+max);
