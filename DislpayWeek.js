9e154a37e9c9f24fa8ce0e2f196c0200ca131fc3
rajashri03 <telvekar.rajashri@gmail.com>
2022-01-21 16:33:03 +0530

    [Rajashr]Add:Read Number and display week using switchcase/ifelse

--- DislpayWeek.js
+++ DislpayWeek.js
@@ -1,6 +1,7 @@
 //Read Number and display the week day
 var readlineSync = require("readline-sync");
 var week = readlineSync.question("Enter Week:");
+//using if else
 if(week == 1)
 {
     console.log("Monday");
@@ -32,4 +33,33 @@ else if(week == 7)
 else
 {
     console.log("Invalid Input! Please enter week number between 1-7.");
+}
+
+//using switch case
+switch(week)
+{
+    case "1":
+        console.log("Monday");
+        break;
+    case "2":
+        console.log("Tuesday");
+        break;
+    case "3":
+        console.log("Wednesday");
+        break;
+    case "4":
+        console.log("Thursday");
+        break;
+    case "5":
+        console.log("Friday");
+        break;
+    case "6":
+        console.log("Saturday");
+        break;
+    case "7":
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid number");
+        break;
 }
